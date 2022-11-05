ebcd0207855d986a1057afbd085d7fa0f6c87be1
Filiberto Medina <darkghost595@gmail.com>
2022-11-04 22:02:18 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data,setData]=useState([])
+  const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=27f915ce5b034dcab3f16802886fd15b`;
+ 
+
+  const button=()=>{
+    axios.get(url)
+    .then(response=> response.data)
+    .then(data=>{
+      console.log(data.results)
+      setData([data.results])
+    }
+    ).catch((error) => {
+      console.log(error);
+   });
+  }
+  
+
+  {console.log(data)}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button  onClick={button}> Search recipe</button>
+          <div className='recipes'>
+            
+          
+
+          {data.map((item) => {
+          return <li>
+            <img src={item[1].image}></img>
+            </li>;
+        })}
+
+          
+          </div>
+         
     </div>
   );
 }
