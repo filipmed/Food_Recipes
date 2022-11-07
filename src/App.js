5a29fb64c593167800d5c276ab14a027f55f15c2
Filiberto Medina <darkghost595@gmail.com>
2022-11-07 00:55:09 -0800

    new form

--- src/App.js
+++ src/App.js
@@ -1,38 +1,60 @@
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState,useMemo } from 'react';
 import './App.css';
 
 function App() {
-  const [data,setData]=useState([])
-  const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=27f915ce5b034dcab3f16802886fd15b`;
+  const [{data,data2},setData]=useState({ data: [], data2: [] })
+  const urls = [`https://api.spoonacular.com/recipes/complexSearch?number=5223&apiKey=27f915ce5b034dcab3f16802886fd15b`,`https://api.spoonacular.com/recipes/informationBulk?ids=715538,716429&apiKey=27f915ce5b034dcab3f16802886fd15b`];
  
 
+  useEffect(()=>{
+        const fetchData = async()=>{
+          const datarep= await axios.get(`https://api.spoonacular.com/recipes/complexSearch?number=5223&apiKey=e82171ec37ef4732842db3c51540289b`);
+          const data2rep = await axios.get(`https://api.spoonacular.com/recipes/informationBulk?ids=715538,716429&apiKey=e82171ec37ef4732842db3c51540289b`)
+          setData({ data: datarep.data, data2: data2rep.data })
+        }
+        fetchData();
+       // eslint-disable-next-line react-hooks/exhaustive-deps 
+  },[])
+/*
   const button=()=>{
-    axios.get(url)
-    .then(response=> response.data)
-    .then(data=>{
-      console.log(data.results)
-      setData(data.results)
-    }
-    ).catch((error) => {
-      console.log(error);
-   });
-  }
+    const promises=urls.map(url=>axios.get(url));
+    Promise.all(promises)
+    .then((response)=>{
+      let data = [];
+      response.forEach(response => {
+        data = data.concat(response.data);
+    });
+
+       setData(data);
+      
+      console.log(data)
+    })
+  } */
   
 
-  {console.log(data)}
+  const filteredPosts = useMemo(() => {
+    const filteredPosts = [];
+    if (data.results)
+        for (let i = 0; i < data.results.length && i < 5224; i++) {
+            filteredPosts.push(data.results[i]);
+        }
+    return filteredPosts;
+}, [data.results]);
+console.log(filteredPosts)
   return (
     <div className="App">
-        <button  onClick={button}> Search recipe</button>
+      
           <div className='recipes'>
             
           
             <ul>
-          {data.map((item) => {
-          return <li key={item.id}>
+          {filteredPosts.map((item) => {
+          return <div className='fooditem' key={item.id}><li >
             <h2>{item.title}</h2>
             <img src={item.image}></img>
-            </li>;
+            
+            </li></div>;
         })}
         </ul>
 
